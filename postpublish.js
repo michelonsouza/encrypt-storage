a8abbe6fd4a7a37b9dad6e035f5af367e5a4b3e2
Michelon Souza <michelon.souza@hotmail.com>
2024-12-29 13:01:52 -0300

    chore(vite): change vite folder exports

--- postpublish.js
+++
@@ -1,8 +0,0 @@
-import fs from 'node:fs';
-
-const data = fs.readFileSync('./package.txt', { encoding: 'utf8', flag: 'r' });
-
-fs.writeFileSync('./package.json', data, {
-  flag: 'w',
-  encoding: 'utf8',
-});
